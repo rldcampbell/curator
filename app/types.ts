68ffcd3329d7eedba4c5fe01bb628b8b71b131cd
Robbie Campbell <rldcampbell@gmail.com>
2025-03-13 15:21:45 +0000

    add prettier / init type for collections

---
+++ app/types.ts
@@ -0,0 +1,44 @@
+type CollectionId = `c${number}`
+type FieldId = `f${number}`
+type ItemId = `i${number}`
+
+type DateArray = [y: number, m: number, d: number]
+
+type BaseField = {
+  id: FieldId
+  name: string
+}
+
+type TextField = BaseField & {
+  type: 'text'
+  charLim: number
+}
+
+type NumberField = BaseField & {
+  type: 'number'
+  min?: number
+  max?: number
+  // format?: NumberFormat // TODO: determin what this should look like - scientific, SF, DP etc. etc.
+}
+
+type DateField = BaseField & {
+  type: 'date'
+  min?: DateArray
+  max?: DateArray
+}
+
+type Field = TextField | NumberField | DateField
+
+type Item = {
+  id: ItemId
+  fieldValues: Record<FieldId, any>
+}
+
+type Collection = {
+  id: CollectionId
+  name: string
+  fieldOrder: FieldId[]
+  fields: Record<FieldId, Field>
+  itemOrder: ItemId[]
+  items: Record<ItemId, Item>
+}
